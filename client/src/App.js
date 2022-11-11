404f09ec724bea361372aa6dd4aff8fc0eba2592
Yolis <yolisisabel28@gmail.com>
2022-11-10 21:07:33 -0800

    routes directing you to its own page working now

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,10 @@ import { useAuth0 } from "@auth0/auth0-react";
 import Loading from "./components/loading";
 import { Route, Routes, Link } from "react-router-dom";
 import Home from "./pageviews/login-page";
-import favdrinksform from "./components/favdrinksform";
+import Favdrinksform from "./components/favdrinksform";
+import HotCoffeeData from "./components/hotcoffee";
+import IceCoffeeData from "./components/icecoffee";
+import Navbarlist from "./components/navbarlist";
 
 function App() {
   const { isLoading } = useAuth0();
@@ -21,12 +24,28 @@ function App() {
         {!user ? (
           <span>Hello from Techtonica</span>
         ) : (
-          <span>
-            Hello <Link to="api/me">{user.name}</Link>
-          </span>
+          <>
+            <span>
+              Hello <Link to="api/me">{user.name}</Link>
+            </span>
+            <Navbarlist />
+          </>
         )}
         <Routes>
           <Route path="/" element={<Home user={user} />} />
+          {/* //conditional rendering: if this is true, make this appear, if false do nothing  */}
+
+          {user && (
+            <>
+              {/* //this will allow to change the nav bar state  */}
+              <Route path="/hotcoffee" element={<HotCoffeeData />} />
+              <Route path="/icecoffee" element={<IceCoffeeData />} />
+              <Route path="/mydrinks" element={<Favdrinksform />} />
+            </>
+          )}
+          {/* //when the user goes to the hot coffee page, we tell the home components
+          to display hot coffee list  */}
+
           <Route path="api/me" element={<Profile user={user} />} />
         </Routes>
       </div>
